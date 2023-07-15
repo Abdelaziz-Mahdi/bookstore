7eada641304cf9c331ca82b0fa948c2069deb2d3
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-15 21:18:36 +0300

    Modify BookList to render books in Book component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,68 +1,45 @@
 import React, { useState } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+import Book from './Book';
 
 export default function BookList() {
-    const [books, setBooks] = useState([
-        {
-            id: 1,
-            title: 'The Hunger Games',
-            category: 'Action',
-            author: 'Suzanne Collins',
-            progress: '64%',
-            chapter: 'Chapter 17',
-            
-        },
-        {
-            id: 2,
-            title: 'Dune',
-            category: 'Sci-Fi',
-            author: 'Frank Herbert',
-            progress: '8%',
-            chapter: 'Chapter 3: "A Lesson Learned"',
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      title: 'The Hunger Games',
+      category: 'Action',
+      author: 'Suzanne Collins',
+      progress: '64%',
+      chapter: 'Chapter 17',
 
-        },
-        {
-            id: 3,
-            title: 'The Lord of the Rings',
-            category: 'History',
-            author: 'J.R.R. Tolkien',
-            progress: '0%',
-            chapter: 'Introduction',
+    },
+    {
+      id: 2,
+      title: 'Dune',
+      category: 'Sci-Fi',
+      author: 'Frank Herbert',
+      progress: '8%',
+      chapter: 'Chapter 3: "A Lesson Learned"',
 
-        },
-    ]);
-   
-    console.log(books);
-    return (
-        <div>
-            <ul>
-                {books.map((book) => (
-                    <li key={book.id}>
-                        <div className="book">
-                            <div className="book-info">
-                                <div className="book-category">{book.category}</div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-author">{book.author}</div>
-                                <div className="book-actions">
-                                    <button type="button">Comments</button>
-                                    <button type="button">Remove</button>
-                                    <button type="button">Edit</button>
-                                </div>
-                            </div>
-                            <div className="book-progress">
-                                <div className="book-progress-circle">
-                                    <div className="book-progress-circle-percentage">{book.progress}</div>
-                                </div>
-                                <div className="book-progress-status">
-                                    <div className="book-progress-status-current">CURRENT CHAPTER</div>
-                                    <div className="book-progress-status-chapter">{book.chapter}</div>
-                                    <button type="button">UPDATE PROGRESS</button>
-                                </div>
-                            </div>
-                        </div>
-                        <br />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+    },
+    {
+      id: 3,
+      title: 'The Lord of the Rings',
+      category: 'History',
+      author: 'J.R.R. Tolkien',
+      progress: '0%',
+      chapter: 'Introduction',
+
+    },
+  ]);
+
+  return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <Book key={book.id} id={book.id} category={book.category} title={book.title} author={book.author} progress={book.progress} chapter={book.chapter} />
+        ))}
+      </ul>
+    </div>
+  );
 }
