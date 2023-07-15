7216b490c8528a7663a00ad7099cd7b238d2fc14
John DoeAbdelaziz <abdelaziz.dv@gmail.com>
2023-07-15 18:41:52 +0300

    Add react router to app component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from './components/Home';
+import Nav from './components/Nav';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav className="App" />}>
+          <Route index element={<Books />} />
+          <Route path="categories" />
+          <Route path="*" element={<div>Page not found 404!</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
