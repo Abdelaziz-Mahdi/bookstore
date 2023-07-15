62c7c23996b19b0c7fa61c032894600a29d77d8c
John DoeAbdelaziz <abdelaziz.dv@gmail.com>
2023-07-15 18:43:01 +0300

    Remove react-dom and use createRoot to render app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
