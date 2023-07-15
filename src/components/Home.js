a2b73907f0bd31fea949199f1978e50504180245
John DoeAbdelaziz <abdelaziz.dv@gmail.com>
2023-07-15 18:39:55 +0300

    Create home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BookList from "./BookList";
+
+export default function Books() {
+  return (
+    <div>
+      <h1>Books</h1>
+        <BookList />
+    </div>
+  );
+}
