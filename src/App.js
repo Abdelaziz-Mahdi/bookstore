e42266db1bfe9a08b670ee500b8366fe6a06165c
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-18 18:02:40 +0300

    Wrap App with the Provider and Pass store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Books from './components/Home';
 import Nav from './components/Nav';
 import Categories from './components/Categories';
+import store from './redux/store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Nav className="App" />}>
-          <Route index element={<Books />} />
-          <Route path="categories" element={<Categories />} />
-          <Route path="*" element={<div>Page not found 404!</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav className="App" />}>
+            <Route index element={<Books />} />
+            <Route path="categories" element={<Categories />} />
+            <Route path="*" element={<div>Page not found 404!</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
