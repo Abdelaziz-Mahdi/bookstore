f40e922f47d1b5fb69ca93d9afa3595f53bbf30f
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-19 06:23:12 +0300

    Remove booklist old code  that replaced by redux

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,67 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 import AddBook from './AddBook';
 
 export default function BookList() {
   const booksList = useSelector((state) => state.books);
-
-  const [books, setBooks] = useState([]);
-
-  // if there is no books in local storage, set the default books
-  useEffect(() => {
-    const getBooks = JSON.parse(localStorage.getItem('books'));
-    if (getBooks.length !== 0) {
-      setBooks(getBooks);
-    } else {
-      setBooks([
-        {
-          id: uuidv4(),
-          title: 'The Hunger Games',
-          category: 'Action',
-          author: 'Suzanne Collins',
-          progress: '64%',
-          chapter: 'Chapter 17',
-
-        },
-        {
-          id: uuidv4(),
-          title: 'Dune',
-          category: 'Sci-Fi',
-          author: 'Frank Herbert',
-          progress: '8%',
-          chapter: 'Chapter 3: "A Lesson Learned"',
-
-        },
-        {
-          id: uuidv4(),
-          title: 'The Lord of the Rings',
-          category: 'History',
-          author: 'J.R.R. Tolkien',
-          progress: '0%',
-          chapter: 'Introduction',
-
-        },
-      ]);
-    }
-  }, []);
-
-  // update the local storage when the books state changes after adding a book
-  const toAddBook = (book) => {
-    const add = [...books, book];
-    localStorage.setItem('books', JSON.stringify(add));
-    const getBooks = JSON.parse(localStorage.getItem('books'));
-    setBooks(getBooks);
-  };
-
-  // update the local storage when the books state changes after deleting a book
-  const toDeleteBook = (id) => {
-    const newBooks = books.filter((book) => book.id !== id);
-    localStorage.setItem('books', JSON.stringify(newBooks));
-    const getBooks = JSON.parse(localStorage.getItem('books'));
-    setBooks(getBooks);
-  };
 
   return (
     <div>
@@ -73,13 +16,10 @@ export default function BookList() {
             category={book.category}
             title={book.title}
             author={book.author}
-            progress={book.progress}
-            chapter={book.chapter}
-            updateBooks={toDeleteBook}
           />
         ))}
       </ul>
-      <AddBook toAddBook={toAddBook} />
+      <AddBook />
     </div>
   );
 }
