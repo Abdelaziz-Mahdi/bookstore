5efaa062f36088b2c1bd43ab245855dcb530b86c
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-19 06:30:16 +0300

    Correct slice reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,19 +2,19 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [
   {
-    item_id: 'item1',
+    id: 'item1',
     title: 'The Great Gatsby',
     author: 'John Smith',
     category: 'Fiction',
   },
   {
-    item_id: 'item2',
+    id: 'item2',
     title: 'Anna Karenina',
     author: 'Leo Tolstoy',
     category: 'Fiction',
   },
   {
-    item_id: 'item3',
+    id: 'item3',
     title: 'The Selfish Gene',
     author: 'Richard Dawkins',
     category: 'Nonfiction',
@@ -26,11 +26,9 @@ const booksSlice = createSlice({
   initialState,
   reducers: {
     addABook: (state, action) => {
-      state.books = state.books.concat(action.payload);
+      state.push(action.payload);
     },
-    removeABook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
-    },
+    removeABook: (state, action) => state.filter((book) => book.id !== action.payload),
   },
 });
 
