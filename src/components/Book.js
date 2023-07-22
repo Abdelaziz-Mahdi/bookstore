89e56f5a7640f3be1b21380ee3a8214f3759f3b8
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-22 12:10:49 +0300

    Modify remove button to dispatch API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeABook } from '../redux/books/booksSlice';
+import { removeBook } from '../redux/books/booksSlice';
 
 const Book = ({
   category, title, author, id,
 }) => {
   const dispatch = useDispatch();
-
-  function removeHandeler(e) {
-    dispatch(removeABook(e.target.parentNode.parentNode.parentNode.parentNode.id));
-  }
-
   return (
-    <li id={id}>
+    <li>
       <div className="book">
         <div className="book-info">
           <div className="book-category">{category}</div>
@@ -21,7 +16,7 @@ const Book = ({
           <div className="book-author">{author}</div>
           <div className="book-actions">
             <button type="button">Comments</button>
-            <button type="button" onClick={removeHandeler}>Remove</button>
+            <button id={id} type="button" onClick={(e) => dispatch(removeBook(e.target.id))}>Remove</button>
             <button type="button">Edit</button>
           </div>
         </div>
