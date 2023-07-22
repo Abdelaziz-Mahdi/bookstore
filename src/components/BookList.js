7adc1ede392bc6c3c963a9e67f31e7a334311cb3
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-22 12:13:22 +0300

    Add condition to display books depending on status

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,34 +1,51 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
-import { selectBooks, fetchBooks } from '../redux/books/booksSlice';
+import {
+  selectBooks, selectBooksError, selectBooksLoading,
+} from '../redux/books/booksSlice';
 
 export default function BookList() {
   const booksList = useSelector(selectBooks);
-  const dispatch = useDispatch();
+  const status = useSelector(selectBooksLoading);
+  const error = useSelector(selectBooksError);
 
-  useEffect(() => {
-    dispatch(fetchBooks());
-  }, [dispatch]);
+  let content;
 
-  const bookData = Object.entries(booksList).map((book) => {
-    const [id, data] = book;
-    return [id, ...data];
-  });
+  if (status === true) {
+    content = (
+      <li>
+        <h2>Loading...</h2>
+      </li>
+    );
+  } else if (status === false) {
+    const bookData = Object.entries(booksList).map((book) => {
+      const [id, data] = book;
+      return [id, ...data];
+    });
+    content = bookData.map((book) => (
+      <Book
+        key={book[0]}
+        id={book[0]}
+        category={book[1].category}
+        title={book[1].title}
+        author={book[1].author}
+      />
+    ));
+  } else if (status === 'failed') {
+    content = (
+      <li>
+        <h2>Posts not found</h2>
+        <p>{error}</p>
+      </li>
+    );
+  }
 
   return (
     <div>
       <ul>
-        {bookData.map((book) => (
-          <Book
-            key={book[0]}
-            id={book[0]}
-            category={book[1].category}
-            title={book[1].title}
-            author={book[1].author}
-          />
-        ))}
+        {content}
       </ul>
       <AddBook />
     </div>
