bb7038b8dcfa20d9fe6fe01457043adc39e4c610
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-22 04:12:01 +0300

    Modify Bookslist to display list of books from API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
+import { selectBooks, fetchBooks } from '../redux/books/booksSlice';
 
 export default function BookList() {
-  const booksList = useSelector((state) => state.books);
+  const booksList = useSelector(selectBooks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
+  const bookData = Object.entries(booksList).map((book) => {
+    const [id, data] = book;
+    return [id, ...data];
+  });
 
   return (
     <div>
       <ul>
-        {booksList.map((book) => (
+        {bookData.map((book) => (
           <Book
-            key={book.id}
-            id={book.id}
-            category={book.category}
-            title={book.title}
-            author={book.author}
+            key={book[0]}
+            id={book[0]}
+            category={book[1].category}
+            title={book[1].title}
+            author={book[1].author}
           />
         ))}
       </ul>
