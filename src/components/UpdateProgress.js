49e6bc14d5d4843ecb432ebc652d6d45ca58bd66
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-23 08:40:20 +0300

    Fix button padding and alignment

--- src/components/UpdateProgress.js
+++ src/components/UpdateProgress.js
@@ -3,11 +3,11 @@ import '../styles/UpdateProgress.css';
 
 const UpdateProgress = () => (
   <div className="col-sm-3 mb-3 mb-sm-0 ps-3 ms-5">
-    <div className="">
+    <div className="ps-3 ms-5">
       <div className="card-body">
         <p className="card-text"><small className="text-body-secondary">Current Chapter</small></p>
         <p className="card-text">Chapter 17</p>
-        <button type="button" className="c-white-two bc-azure">Update progress</button>
+        <button type="button" className="c-white-two bc-azure  ps-4 pe-4">Update progress</button>
       </div>
     </div>
   </div>
