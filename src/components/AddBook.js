65079eec5e1a93bb3231609717830d34c32dac38
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-27 23:39:22 +0300

    Update add button size and padding

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -38,21 +38,21 @@ const AddBook = () => {
     <div className="container p-3">
       <form className="t-border pt-3" onSubmit={handleSubmit}>
         <h5 className="text-body-secondary">Add New Book</h5>
-        <label htmlFor="title" className="col-5 me-2">
+        <label htmlFor="title" className="col-4 me-2">
           <input required className="form-control" placeholder="Book title" aria-label="Book title" type="text" name="title" id="title" value={book.title} onChange={handleChange} />
         </label>
         <label htmlFor="author" className="col-3 m-2">
           <input required className="form-control" placeholder="Author name" aria-label="Author name" type="text" name="author" id="author" value={book.author} onChange={handleChange} />
         </label>
-        <label htmlFor="category" className="col-3 ms-2">
-          <select name="category" placeholder="Author name" id="category" value={book.category} onChange={handleChange}>
+        <label htmlFor="category" className="col-4 ms-2">
+          <select className="col-5" name="category" placeholder="Author name" id="category" value={book.category} onChange={handleChange}>
             <option value="Select Category" disabled defaultValue>Category</option>
             <option value="General">General</option>
             <option value="Action">Action</option>
             <option value="Sci-Fi">Sci-Fi</option>
             <option value="History">History</option>
           </select>
-          <button className="c-white-two bc-azure ms-1 ps-4 pe-4" type="submit">ADD BOOK</button>
+          <button className="c-white-two bc-azure ms-1 col-5" type="submit">ADD BOOK</button>
         </label>
       </form>
     </div>
