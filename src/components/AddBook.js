b2d5ffe104118f4c0be0d3b7394a023005ae153b
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-28 07:16:59 +0300

    Adjust add book mobile view

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -36,23 +36,23 @@ const AddBook = () => {
   };
   return (
     <div className="container p-3">
-      <form className="t-border pt-3" onSubmit={handleSubmit}>
+      <form className="t-border pt-3 text-center" onSubmit={handleSubmit}>
         <h5 className="text-body-secondary">Add New Book</h5>
-        <label htmlFor="title" className="col-4 me-2">
+        <label htmlFor="title" className="col-md-4 col-12 me-2 pb-2">
           <input required className="form-control" placeholder="Book title" aria-label="Book title" type="text" name="title" id="title" value={book.title} onChange={handleChange} />
         </label>
-        <label htmlFor="author" className="col-3 m-2">
+        <label htmlFor="author" className="col-md-3 col-12 m-md-2 pb-2">
           <input required className="form-control" placeholder="Author name" aria-label="Author name" type="text" name="author" id="author" value={book.author} onChange={handleChange} />
         </label>
-        <label htmlFor="category" className="col-4 ms-2">
-          <select className="col-5" name="category" placeholder="Author name" id="category" value={book.category} onChange={handleChange}>
+        <label htmlFor="category" className="col-md-4 col-12 ms-md-2 text-center pb-2">
+          <select className="col-6 col-md-5" name="category" placeholder="Author name" id="category" value={book.category} onChange={handleChange}>
             <option value="Select Category" disabled defaultValue>Category</option>
             <option value="General">General</option>
             <option value="Action">Action</option>
             <option value="Sci-Fi">Sci-Fi</option>
             <option value="History">History</option>
           </select>
-          <button className="c-white-two bc-azure ms-1 col-5" type="submit">ADD BOOK</button>
+          <button className="c-white-two bc-azure ms-4 ms-md-1 col-5" type="submit">ADD BOOK</button>
         </label>
       </form>
     </div>
