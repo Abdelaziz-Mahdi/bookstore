33eacd05fcdaea005894b21f47ccbd0a2ec384d7
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-29 09:54:37 +0300

    Correct font size and padding for completed comp

--- src/components/Completed.js
+++ src/components/Completed.js
@@ -3,13 +3,13 @@ import '../styles/Completed.css';
 
 const Completed = () => (
   <div className="col-sm-3 mb-3 mb-sm-0 r-border d-flex flex-column justify-content-center">
-    <div className="row">
+    <div className="row ps-3">
       <div className="col-md-4">
         <div className="progress-bar" />
       </div>
-      <div className="col-md-8">
+      <div className="col-md-8 ps-md-4 ps-lg-0">
         <div className="card-body">
-          <h5 className="card-title">64%</h5>
+          <h3 className="card-title">64%</h3>
           <p className="card-text"><small className="text-body-secondary">Completed</small></p>
         </div>
       </div>
