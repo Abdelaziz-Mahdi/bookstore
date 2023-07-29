ff451c54073b50102356f25b71cf1b195b85a075
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-29 10:16:01 +0300

    Fix update progress padding on different ss

--- src/components/UpdateProgress.js
+++ src/components/UpdateProgress.js
@@ -2,8 +2,8 @@ import React from 'react';
 import '../styles/UpdateProgress.css';
 
 const UpdateProgress = () => (
-  <div className="col-sm-3 mb-3 mb-sm-0 ps-3 ms-5">
-    <div className="ps-3 ms-5">
+  <div className="col-sm-3 mb-3 mb-sm-0 ps-0 ps-sm-1 ps-md-2 ps-lg-5">
+    <div className="ps-md-3 ps-lg-1 ps-xl-3">
       <div className="card-body">
         <p className="card-text"><small className="text-body-secondary">Current Chapter</small></p>
         <p className="card-text">Chapter 17</p>
